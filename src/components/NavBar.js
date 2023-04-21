3c0668a56e5ef1a8ef48ef04d0d2f7a21a83c5ad
ShirshakShahi <royalshirshal000@gmail.com>
2023-04-21 12:57:55 +0545

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,18 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <div>
+            <ul className="cont">
+                <li>Home</li>
+                <li>About</li>
+                <li>Services</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+
+
+    );
+}
+
+export default NavBar;
