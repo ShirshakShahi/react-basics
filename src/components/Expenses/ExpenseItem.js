012e7380eebce38ef68a75054a5da905e9ac6b1f
ShirshakShahi <royalshirshal000@gmail.com>
2023-04-22 19:21:45 +0545

    returning conditional jsx files

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React,{ useState } from 'react';
+import React, { useState } from 'react';
 
 import './ExpenseItem.css';
 import './ExpenseDate';
@@ -7,22 +7,24 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-    const [title,setTitle] = useState(props.title);
+    const [title, setTitle] = useState(props.title);
 
-    const clickHandler = () =>{
+    const clickHandler = () => {
         setTitle('Updated!!!');
         console.log(title);
     }
     return (
-        <Card className="expense-item">
-            <ExpenseDate date={props.date} />
+        <li> 
+            <Card className="expense-item">
+                <ExpenseDate date={props.date} />
 
-            <div className="expense-item__description">
-                <h2>{title}</h2>
-                <div className="expense-item__price">$ {props.amount}</div>
-            </div>
-            <button onClick = {clickHandler} >Change Title</button>
-        </Card>
+                <div className="expense-item__description">
+                    <h2>{title}</h2>
+                    <div className="expense-item__price">$ {props.amount}</div>
+                </div>
+                <button onClick={clickHandler} >Change Title</button>
+            </Card>
+        </li>
     );
 }
 export default ExpenseItem;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import ExpensesFilter from '../Expenses/ExpenseFilter';
 import './Expenses.css';
+import ExpenseList from './ExpenseList';
 
 const Expenses = (props) => {
 
@@ -19,27 +19,12 @@ const Expenses = (props) => {
     });
 
 
-    let expenseFirst = <p>No Expenses Found</p>;
-
-    if (filteredExpenses.length > 0) {
-
-        expenseFirst = filteredExpenses.length > 0 && (
-            filteredExpenses.map(expense => <ExpenseItem
-                key={expense.id}
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} />)
-        )
-
-    }
-
-
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-            {expenseFirst}
+            <ExpenseList items = {filteredExpenses}/>
 
         </Card>
     );
