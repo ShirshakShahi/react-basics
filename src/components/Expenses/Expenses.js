0c125e6f98b30926b3127f1ca433e5e50d61babc
ShirshakShahi <royalshirshal000@gmail.com>
2023-04-22 17:53:08 +0545

    jsx saved in variable,ternary operators,array filters

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
-import ExpensesFilter from './ExpenseFilter';
+import ExpensesFilter from '../Expenses/ExpenseFilter';
 import './Expenses.css';
 
 const Expenses = (props) => {
@@ -11,17 +11,35 @@ const Expenses = (props) => {
     const filterChangeHandler = (selectedYear) => {
 
         setFilteredYear(selectedYear);
-  
+
     }
+
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+
+    let expenseFirst = <p>No Expenses Found</p>;
+
+    if (filteredExpenses.length > 0) {
+
+        expenseFirst = filteredExpenses.length > 0 && (
+            filteredExpenses.map(expense => <ExpenseItem
+                key={expense.id}
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date} />)
+        )
+
+    }
+
+
 
     return (
         <Card className="expenses">
             <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
 
-            {props.items.map(expense => <ExpenseItem
-                title={expense.title}
-                amount={expense.amount}
-                date={expense.date} />)}
+            {expenseFirst}
 
         </Card>
     );
